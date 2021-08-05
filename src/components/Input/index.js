d6f4c773de00e48a1df87771ec71802870023d0b
gabriel.jesus <gabriel.jesus@globalsys.com>
2021-08-05 10:31:52 -0300

    component input e funcionalidade do campo

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,7 +4,11 @@ import { TextInput } from "react-native";
 import styles from "./styles";
 
 const Input = ({ style, ...rest }) => {
-  return <TextInput {...rest} style={{ ...styles.input, ...style }} />;
+  return (
+    <>
+      <TextInput {...rest} style={{ ...styles.input, ...style }} />
+    </>
+  );
 };
 
 export default Input;
--- src/pages/StartGameScreen/index.js
+++ src/pages/StartGameScreen/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 
 import Card from "../../components/Card";
@@ -8,6 +8,7 @@ import Colors from "../../constants/color";
 import styles from "./styles";
 
 const StartGameScreen = () => {
+  const [enteredValue, setEnteredValue] = useState("");
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar um novo jogo!</Text>
@@ -20,6 +21,8 @@ const StartGameScreen = () => {
           autoCorrect={false}
           keyBoardType="numeric"
           maxLength={2}
+          value={enteredValue}
+          onChangeText={(text) => setEnteredValue(text)}
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
