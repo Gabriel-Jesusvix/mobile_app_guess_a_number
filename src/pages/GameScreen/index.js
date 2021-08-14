f6cd505bea6250586237992d04b890662a8201c2
gabriel.jesus <gabriel.jesus@globalsys.com>
2021-08-14 12:18:46 -0300

    initial screen

--- src/pages/GameScreen/index.js
+++ src/pages/GameScreen/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
 
-const GameScreen = () => {
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+};
+const GameScreen = ({ userChoice }) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, userChoice)
+  );
   return (
     <View style={styles.container}>
       <Text>GameScreen</Text>
